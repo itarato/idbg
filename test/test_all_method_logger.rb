173d2357fda3cf6a412808ac5301bcb273e445d4
Peter Arato <it.arato@gmail.com>
2022-09-10 17:28:16 -0400

    improve fun logger

---
+++ test/test_all_method_logger.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require_relative("../i_dbg.rb")
+
+class Foo
+  def initialize
+    alpha(12)
+  end
+
+  def alpha(i)
+    beta(foo: 123)
+  end
+
+  def beta(args)
+    gamma(:a, :b, :c)
+  end
+
+  def gamma(*args)
+  end
+
+  include(IDbg.function_logger.with_args)
+end
+
+class Bar
+  def initialize
+    alpha(12)
+  end
+
+  def alpha(i)
+    beta(foo: 123)
+  end
+
+  def beta(args)
+    gamma(:a, :b, :c)
+  end
+
+  def gamma(*args)
+  end
+
+  include(IDbg.function_logger)
+end
+
+Foo.new
+Bar.new
